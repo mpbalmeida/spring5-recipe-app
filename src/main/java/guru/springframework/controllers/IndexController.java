9066b918da5e200c3226562f2b360208c74b2fbd
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-04-02 00:15:29 -0300

    JPA Query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnityOfMeasure;
+import guru.springframework.repository.CategoryRepository;
+import guru.springframework.repository.UnityOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnityOfMeasureRepository unityOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnityOfMeasureRepository unityOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unityOfMeasureRepository = unityOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnityOfMeasure> unityOfMeasureOptional = unityOfMeasureRepository.findByDescription("Cup");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unityOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/guru/springframework/repository/UnityOfMeasureRepository.java
+++ src/main/java/guru/springframework/repository/UnityOfMeasureRepository.java
@@ -3,5 +3,9 @@ package guru.springframework.repository;
 import guru.springframework.domain.UnityOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnityOfMeasureRepository extends CrudRepository<UnityOfMeasure, Long> {
+
+    Optional<UnityOfMeasure> findByDescription(String description);
 }
