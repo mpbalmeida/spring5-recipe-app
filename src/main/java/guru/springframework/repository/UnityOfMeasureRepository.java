2b9ac907fe47fb2a0d0a8a524a1f48e04cba541e
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-04-02 00:06:37 -0300

    Creating repositories

---
+++ src/main/java/guru/springframework/repository/UnityOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repository;
+
+import guru.springframework.domain.UnityOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnityOfMeasureRepository extends CrudRepository<UnityOfMeasure, Long> {
+}
