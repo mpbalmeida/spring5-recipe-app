7c7d41cf28bdeedab19c89f3efca775b4c32c23c
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-06-30 19:52:18 -0300

    Adding recipes view

---
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -0,0 +1,30 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Marcos Almeida on 30/06/2018
+ */
+@Slf4j
+@Controller
+public class RecipeController {
+
+    RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model) {
+
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+
+        return "recipe/show";
+    }
+}
