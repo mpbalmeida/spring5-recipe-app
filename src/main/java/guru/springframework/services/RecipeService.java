74ae608c54a8c77aca4e8ff837ca6465d20da0eb
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-07-05 19:34:57 -0300

    Adding save recipe command

--- src/main/java/guru/springframework/services/RecipeService.java
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
 
 import java.util.Optional;
@@ -9,4 +10,5 @@ public interface RecipeService {
 
     Set<Recipe> getRecipes();
     Recipe findById(Long id);
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
