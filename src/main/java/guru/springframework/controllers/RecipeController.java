5a3229c0d8699e647453042f8c281d5d14e8e1c1
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-07-16 20:16:38 -0300

    Adding NFE handling globally with controller advice.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -77,18 +77,4 @@ public class RecipeController {
         return modelAndView;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormException(Exception exception) {
-
-        log.error("Handling not found exception");
-        log.error(exception.getMessage());
-
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }
 }
